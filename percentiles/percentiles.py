6bc641702d01db757d8619a9e190f058e39d2670
Kalu <129804522+KaluArunsi@users.noreply.github.com>
2026-01-03 22:41:58 +0800

    Update solution for percentiles

--- percentiles/percentiles.py
+++ percentiles/percentiles.py
@@ -4,9 +4,7 @@ def percentiles(x, q):
     """
     Compute percentiles using linear interpolation.
     """
-    x, q = np.array(x), np.array(q)
-
-    #edge_case = np.where()
+    x, q = np.atleast_1d(x), np.atleast_1d(q)
 
     output = np.percentile(x, q)
 
