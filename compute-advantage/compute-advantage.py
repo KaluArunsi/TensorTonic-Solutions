59a8ee9037bd3429b88d6b3b81665a24eda11162
Kalu <129804522+KaluArunsi@users.noreply.github.com>
2026-01-18 16:40:40 +0800

    Add solution for compute-advantage

---
+++ compute-advantage/compute-advantage.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def compute_advantage(states, rewards, V, gamma):
+    """
+    Returns: A (NumPy array of advantages)
+    """
+    V, rewards = np.asanyarray(V), np.asanyarray(rewards)
+
+    g_t = np.zeros_like(rewards, dtype=float)
+    v_st = []
+    g_value = 0
+
+    for r in reversed(range(len(rewards))):
+        # apparently you can recursively just do
+        # G_current = r + (gamma * G_previous)
+        g_value = rewards[r] + (gamma * g_value)
+        g_t[r] = g_value
+        
+    advantages = g_t - V
+
+    return advantages
+
