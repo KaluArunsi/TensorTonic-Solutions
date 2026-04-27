2ba2728f6ecaaf1aaa39c1ec4ba34d5bb4255ed6
Kalu <129804522+KaluArunsi@users.noreply.github.com>
2026-04-27 10:38:00 +0800

    Add solution for la-dot-product

---
+++ study-plans/math-linear-algebra/la-dot-product/la-dot-product.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def dot_product(x, y):
+    """
+    Compute the dot product of two 1D arrays x and y.
+    Must return a float.
+    """
+    x = np.array(x, dtype='float64')
+    y = np.array(y, dtype='float64')
+
+    dot = np.dot(x, y)
+
+    return dot
