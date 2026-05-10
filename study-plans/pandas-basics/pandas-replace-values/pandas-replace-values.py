888c08a5f86b2c412ca4f1ea106b48cfba6727b3
Kalu <129804522+KaluArunsi@users.noreply.github.com>
2026-05-10 09:37:58 +0800

    Add solution for pandas-replace-values

---
+++ study-plans/pandas-basics/pandas-replace-values/pandas-replace-values.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def replace_values(data, column, old_val, new_val):
+    """
+    Returns: dict with 'data' (dict) and 'count' (int)
+    """
+    df = pd.DataFrame(data)
+
+    count_target = int((df[column] == old_val).sum())
+
+    df[column].replace(old_val, new_val, inplace=True)
+
+    return {
+        'data': df.to_dict(orient='list'),
+        'count': count_target
+    }
