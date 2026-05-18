2ed86792714d5ec4b8c081c9a631a296daf65ca9
Kalu <129804522+KaluArunsi@users.noreply.github.com>
2026-05-18 21:58:55 +0800

    Add solution for pytorch-dropout-from-scratch

---
+++ study-plans/pytorch-basics/pytorch-dropout-from-scratch/pytorch-dropout-from-scratch.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class Dropout(nn.Module):
+    def __init__(self, p=0.5):
+        super().__init__()
+        self.p = p
+
+    def forward(self, x):
+        """
+        Returns: tensor with dropout applied
+        """
+        if not self.training or self.p == 0.0:
+            return x
+
+        elif self.p == 1.0:
+            return torch.zeros(x.shape)
+
+        else:
+            prob_tensor = torch.full(x.shape, 1 - self.p)
+            m = torch.bernoulli(prob_tensor)
+    
+            y = (m * x) / (1 - self.p)
+    
+            return y
